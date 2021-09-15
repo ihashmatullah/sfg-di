c8d2aa39ed8831eb74d58ca645234a49d45190a9
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-15 17:10:27 -0500

    Constructor Properties Binding Lec-6 P96

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -6,24 +6,23 @@ import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.*;
 
 /**
  * Created by HashmatN at 4:05 PM on Sep 14, 2021
  */
+@EnableConfigurationProperties(SfgConsturctorConfig.class)
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
   @Bean
-  FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
-                                @Value("${guru.password}") String password,
-                                @Value("${guru.jdbcurl}") String jdbcurl) {
+  FakeDataSource fakeDataSource(SfgConsturctorConfig sfgConsturctorConfig) {
     FakeDataSource fakeDataSource = new FakeDataSource();
-    fakeDataSource.setUsername(username);
-    fakeDataSource.setPassword(password);
-    fakeDataSource.setJdbcurl(jdbcurl);
+    fakeDataSource.setUsername(sfgConsturctorConfig.getUsername());
+    fakeDataSource.setPassword(sfgConsturctorConfig.getPassword());
+    fakeDataSource.setJdbcurl(sfgConsturctorConfig.getJdbcurl());
     return fakeDataSource;
   }
 
